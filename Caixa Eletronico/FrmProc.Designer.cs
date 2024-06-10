77efd2e4cf2093ecb1454198e4126e5001c18010
ra2357083 <ra2357083@LDI317>
2024-06-10 08:22:28 -0300

    duwerupw

--- Caixa Eletronico/FrmProc.Designer.cs
+++ Caixa Eletronico/FrmProc.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            NumValor = new NumericUpDown();
+            label2 = new Label();
+            TxtConta = new TextBox();
+            BtnEnviar = new Button();
+            label1 = new Label();
+            LblOpcao = new Label();
+            ((System.ComponentModel.ISupportInitialize)NumValor).BeginInit();
+            SuspendLayout();
+            // 
+            // NumValor
+            // 
+            NumValor.Location = new Point(154, 94);
+            NumValor.Name = "NumValor";
+            NumValor.Size = new Size(120, 23);
+            NumValor.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(154, 138);
+            label2.Name = "label2";
+            label2.Size = new Size(100, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Conta de destino:";
+            // 
+            // TxtConta
+            // 
+            TxtConta.Location = new Point(154, 156);
+            TxtConta.Name = "TxtConta";
+            TxtConta.Size = new Size(120, 23);
+            TxtConta.TabIndex = 3;
+            // 
+            // BtnEnviar
+            // 
+            BtnEnviar.Location = new Point(154, 210);
+            BtnEnviar.Name = "BtnEnviar";
+            BtnEnviar.Size = new Size(120, 23);
+            BtnEnviar.TabIndex = 4;
+            BtnEnviar.Text = "Enviar";
+            BtnEnviar.UseVisualStyleBackColor = true;
+            BtnEnviar.Click += BtnEnviar_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(154, 76);
+            label1.Name = "label1";
+            label1.Size = new Size(36, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Valor:";
+            // 
+            // LblOpcao
+            // 
+            LblOpcao.AutoSize = true;
+            LblOpcao.Location = new Point(154, 32);
+            LblOpcao.Name = "LblOpcao";
+            LblOpcao.Size = new Size(0, 15);
+            LblOpcao.TabIndex = 6;
+            // 
+            // FrmProc
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(445, 283);
+            Controls.Add(LblOpcao);
+            Controls.Add(label1);
+            Controls.Add(BtnEnviar);
+            Controls.Add(TxtConta);
+            Controls.Add(label2);
+            Controls.Add(NumValor);
+            Name = "FrmProc";
+            Text = "FrmProc";
+            Load += FrmProc_Load;
+            ((System.ComponentModel.ISupportInitialize)NumValor).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private NumericUpDown NumValor;
+        private Label label2;
+        private TextBox TxtConta;
+        private Button BtnEnviar;
+        private Label label1;
+        private Label LblOpcao;
     }
 }
--- Caixa Eletronico/FrmProc.cs
+++ Caixa Eletronico/FrmProc.cs
@@ -4,17 +4,131 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Caixa_Eletronico.Classes;
 
 namespace Caixa_Eletronico
 {
     public partial class FrmProc : Form
     {
-        public FrmProc()
+        bool pode_N;//mudar
+        bool pode_T;//mudar
+        Singleton s;//mudar
+        private string tipo;//mudar
+
+        public FrmProc()//mudar
+        {
+            s = Singleton.Instance;
+        }
+        public FrmProc(string tipo) : this()//mudar
         {
             InitializeComponent();
+            this.tipo = tipo;
         }
+
+        private void FrmProc_Load(object sender, EventArgs e)//mudar
+        {
+            LblOpcao.Text = tipo;
+            if (tipo == "Transferência")
+            {
+                TxtConta.Visible = true;
+                label2.Visible = true;
+            }
+        }
+        private void BtnEnviar_Click(object sender, EventArgs e)//mudar
+        {
+            if (tipo == "Saque")
+            {
+                double valor = (double)NumValor.Value;
+                if (s.conta_logada.Sacar(valor))
+                {
+                    MessageBox.Show("Saque realizado!");
+                }
+                else
+                {
+                    MessageBox.Show("Saldo insuficiente...");
+                }
+            }
+            if (tipo == "Depósito")
+            {
+                double valor = (double)NumValor.Value;
+                s.conta_logada.Depositar(valor);
+                MessageBox.Show("Depósito realizado!");
+            }
+            if (tipo == "Transferência")
+            {
+                double valor = (double)NumValor.Value;
+                Conta destino = s.BuscarConta(TxtConta.Text);
+                if (destino != null)
+                {
+                    if (s.conta_logada.Transferir(destino, valor))
+                    {
+                        MessageBox.Show("Transferência realizada!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Saldo insuficiente...!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Conta destino não existe!");
+                }
+            }
+            frmPrincipal frm = new frmPrincipal();
+            frm.Show();
+            this.Hide();
+        }
+        private void numValor_ValueChanged(object sender, EventArgs e)//mudar
+        {
+            if (tipo != "Transferência")
+            {
+                if (NumValor.Value != 0)
+                {
+                    BtnEnviar.Enabled = true;
+                }
+                else
+                {
+                    BtnEnviar.Enabled = false;
+                }
+            }
+            else
+            {
+                if (NumValor.Value != 0)
+                {
+                    pode_N = true;
+                    CheckEnableBtFazer();
+                }
+                else
+                {
+                    pode_N = false;
+                    CheckEnableBtFazer();
+                }
+            }
+        }
+
+        private void txtConta_TextChanged(object sender, EventArgs e)//mudar
+        {
+            if (TxtConta.Text != "")
+            {
+                pode_T = true;
+                CheckEnableBtFazer();
+            }
+            else
+            {
+                BtnEnviar.Enabled = false;
+            }
+        }
+        private void CheckEnableBtFazer()//mudar
+        {
+            if (tipo == "Transferência")
+            {
+                BtnEnviar.Enabled = pode_N && pode_T;
+            }
+        }
+
     }
 }
